b3651969e93b2b3289df7aed6f6f8eaaad83a7c0
asmajawaid <arshadasma45678@gmail.com>
2024-04-07 23:12:05 +0500

    add fast cash function

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import inquirer from "inquirer";
 //initialize user balance and pin code
 let myBlanace = 10000; //dollar
 let myPin = 1234;
-// print welcome message 
+// print welcome message
 console.log("Welcome to AGB - ATM machine");
 
 let answer = await inquirer.prompt([
@@ -17,16 +17,16 @@ let answer = await inquirer.prompt([
 
 if (answer.userPin === myPin) {
   console.log("your pin code is correct! Login successfully.");
-  console.log(`current balance is ${myBlanace}`)
+  console.log(`current balance is ${myBlanace}`);
 
   let operationAns = await inquirer.prompt([
     {
       name: "operation",
       message: "please select an opetion.",
       type: "list",
-      choices: ["withdraw amount", "check balance"],
+      choices: ["withdraw amount", "check balance", "fast cash"],
     },
-  ])
+  ]);
   if (operationAns.operation === "withdraw amount") {
     let amountAns = await inquirer.prompt([
       {
@@ -35,21 +35,33 @@ if (answer.userPin === myPin) {
         type: "number",
       },
     ]);
-    if (amountAns.amount > myBlanace){
+    if (amountAns.amount > myBlanace) {
       console.log("insufficient Balance");
-    }else{
-    myBlanace -= amountAns.amount;
-    console.log(`${amountAns.amount} withdraw successfully`);
-    console.log(`"your remaining balance is:" ${myBlanace}`);
+    } else {
+      myBlanace -= amountAns.amount;
+      console.log(`${amountAns.amount} withdraw successfully`);
+      console.log(`"your remaining balance is:" ${myBlanace}`);
+    }
+  } else if (operationAns.operation === "check balance") {
+    console.log(`"your  current account balance is:"  ${myBlanace}`);
+  } else if (operationAns.operation === "fast cash") {
+    let fastAmount = await inquirer.prompt([
+      {
+        name: "fastcash",
+        message: "Select your amount you want to withdraw",
+        type: "list",
+        choices: ["10000", "20000", "30000", "40000"],
+      },
+    ]);
+    if (myBlanace >= fastAmount.fastcash) {
+      myBlanace -= fastAmount.fastcash;
+      console.log("Sucessfully withdraw");
+      console.log(`Your remaining balance is: ${myBlanace}`);
+    } else fastAmount.amount > myBlanace;
+    {
+      console.log("insufficient Balance");
+    }
   }
-    
-  }
-  
-  else if (operationAns.operation === "check balance") {
-    console.log(`"your  current account balance is:"  ${myBlanace}`); 
-  }
- 
-  
 } else {
   console.log("PIN is Incorrect.Try again!!!");
 }
